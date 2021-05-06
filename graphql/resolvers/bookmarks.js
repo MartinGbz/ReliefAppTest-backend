4537de414d52a87452c39bb54c0a969d0f0aa180
MartinGbz <martin.grabarz@outlook.fr>
2021-05-06 12:47:28 +0200

    code cleaning

--- graphql/resolvers/bookmarks.js
+++ graphql/resolvers/bookmarks.js
@@ -3,8 +3,7 @@ const Bookmarks = require("../../models/Bookmarks")
 module.exports = {
     bookmarks: async () => {
         try {
-            const bookmarksFetched = await Bookmarks.find()
-            return bookmarksFetched
+            return await Bookmarks.find()
         } catch (error) {
             throw error
         }
@@ -28,8 +27,7 @@ module.exports = {
             const bookmark = new Bookmarks({
                 url
             })
-            const newBookmark = await bookmark.save()
-            return newBookmark
+            return await bookmark.save()
         } catch (error) {
             throw error
         }
